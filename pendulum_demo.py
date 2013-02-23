9d970fc3e4fbd3226672aaea4d0dcde8da1ba977
Keith Batten <kbatten@gmail.com>
2013-02-23 01:40:45 -0800

    add some documentation to pendulum_demo

--- pendulum_demo.py
+++ pendulum_demo.py
@@ -58,41 +58,50 @@ def serve_gif(frames, duration, nq=0):
 
 
 if __name__ == '__main__':
+    # image settings
     supersample = 2
     width = 200
     font_size = 8
 
+    # animation settings
     step = 0.05
     duration = 4
     blur = 0
 
+    # calculate internal size of world
     x = int(supersample*width)
     y = (6 * x / 5)
 
+    # automatically scale font with supersample
     world = World(x, y, Viewport.WHITE, ("/usr/share/fonts/truetype/freefont/FreeSansBold.ttf", supersample * font_size))
 
+    # create and add viewports
     viewport_different = Viewport(-4, 1.9, 8, -0.5, (0,200,0))
     viewport = Viewport(-3, 3, 3, -3, Viewport.BEIGE)
 
     world.add_viewport(viewport, 0, y/6, x, y)
     world.add_viewport(viewport_different, 0, 0, x, y/6)
 
+    # create and add machines
     seconds_pendulum = Pendulum(Point(0,1), Vector.from_polar((2/(2*math.pi)) * (2/(2*math.pi)) * scipy.constants.g, math.radians(320)))
     world.add_machine(seconds_pendulum)
-    seconds2_pendulum = Pendulum(Point(0,2), Vector.from_polar((4/(2*math.pi)) * (4/(2*math.pi)) * scipy.constants.g, math.radians(300)))
-    world.add_machine(seconds2_pendulum)
+    twoseconds_pendulum = Pendulum(Point(0,2), Vector.from_polar((4/(2*math.pi)) * (4/(2*math.pi)) * scipy.constants.g, math.radians(300)))
+    world.add_machine(twoseconds_pendulum)
 
     timer = Timer(Point(2,2))
     world.add_machine(timer)
 
     viewport.add_axis(0.2, 1)
+
+    # add object visualizations to viewports
     viewport.add_visualization(seconds_pendulum.visualization_basic)
-    viewport.add_visualization(seconds2_pendulum.visualization_basic)
+    viewport.add_visualization(twoseconds_pendulum.visualization_basic)
     viewport.add_visualization(timer.visualization_basic)
 
     viewport_different.add_visualization(seconds_pendulum.visualization_different)
-    viewport_different.add_visualization(seconds2_pendulum.visualization_different)
+    viewport_different.add_visualization(twoseconds_pendulum.visualization_different)
 
+    # generate frames
     timer_start = time.time()
     duration = step * math.ceil(duration/step)
     frames = world.get_frames(0, duration, step, blur, 1.0/supersample)
